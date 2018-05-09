cd1f81ca7518fcb9e3bbfefdb3904d5c154246f5
Torben Clasen <torbenclasen@gmail.com>
2018-05-10 00:52:22 +0200

    Added the method to also write out the statements for the Foreign Keys

--- XMLParser/DB/DBTable.cs
+++ XMLParser/DB/DBTable.cs
@@ -16,14 +16,22 @@ namespace XMLParser.DB
             DBFields = fields;
         }
 
-        public override string ToString()
+        public override string ToString() => $"Create table {Name} ({String.Join(",", DBFields)}{KeyStatements()})";
+
+        private string KeyStatements()
         {
-            return $"Create table {Name} ({String.Join(",", DBFields)}{PrimaryKeyStatement()})";
+            var primaryKey = PrimaryKeyStatement();
+            var foreignKey = ForeignKeyStatement();
+            return foreignKey.Length != 0 ? $"{primaryKey} ,{foreignKey}" : primaryKey ;
         }
 
-        private string PrimaryKeyStatement()
+        private string PrimaryKeyStatement() => PrimaryKey != null ? $", Primary key({PrimaryKey.Name})" : string.Empty;
+
+        private string ForeignKeyStatement()
         {
-            return PrimaryKey != null ? $", Primary key({PrimaryKey.Name})" : string.Empty;
+            var foreignKeyDefinitions = DBFields.Where(x => x.DBFieldKeyType == DBFieldKeyType.ForeignKey).Select(x => $"FOREIGN KEY({x.Name}) REFERENCES {x.ForeignKeyReferences[0].Table.Name}({x.ForeignKeyReferences[0].Field.Name})");
+            return String.Join(",", foreignKeyDefinitions);
         }
+
     }
 }
