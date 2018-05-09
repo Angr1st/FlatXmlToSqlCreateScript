dffd7b857a181c000fd6065120816caa8bcfca76
Torben Clasen <torbenclasen@gmail.com>
2018-05-09 20:54:43 +0200

    Added a PrimaryKey Property that is either null or the first PrimaryKey found in the DBFields.

--- XMLParser/DB/DBTable.cs
+++ XMLParser/DB/DBTable.cs
@@ -8,6 +8,7 @@ namespace XMLParser.DB
     {
         public string Name { get; }
         public List<DBField> DBFields { get; }
+        public DBField PrimaryKey { get { return DBFields.Find(x => x.DBFieldKeyType == DBFieldKeyType.PrimaryKey); } }
 
         public DBTable(string name, List<DBField> fields)
         {
@@ -17,7 +18,12 @@ namespace XMLParser.DB
 
         public override string ToString()
         {
-            return $"Create table {Name} ({String.Join(",", DBFields)}, Primary key({DBFields.First().Name}))";
+            return $"Create table {Name} ({String.Join(",", DBFields)}{PrimaryKeyStatement()})";
+        }
+
+        private string PrimaryKeyStatement()
+        {
+            return PrimaryKey != null ? $", Primary key({PrimaryKey.Name})" : string.Empty;
         }
     }
 }
