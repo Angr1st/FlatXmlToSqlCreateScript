e447eb7b01c1f2f9d161c1f8b0c3ea3806e639e6
Torben Clasen <torbenclasen@gmail.com>
2018-06-05 22:09:26 +0200

    Added a way to add the default field.

--- XMLParser/DB/DBTable.cs
+++ XMLParser/DB/DBTable.cs
@@ -7,7 +7,7 @@ namespace XMLParser.DB
     class DBTable
     {
         public string Name { get; }
-        public List<DBField> DBFields { get; }
+        public List<DBField> DBFields { get; private set; }
         public (List<DBField> primaryKeyFields, DBFieldKeyType pkType) PrimaryKey { get { return DBFields.Count(fields => fields.DBFieldKeyType == DBFieldKeyType.PrimaryKey) == 1 ? (DBFields.FindAll(x => x.DBFieldKeyType == DBFieldKeyType.PrimaryKey), DBFieldKeyType.PrimaryKey) : DBFields.Count(fields => fields.DBFieldKeyType.HasFlag( DBFieldKeyType.ClusteredPrimaryKey)) > 1 ? (DBFields.FindAll(x => x.DBFieldKeyType.HasFlag(DBFieldKeyType.ClusteredPrimaryKey)), DBFieldKeyType.ClusteredPrimaryKey) : default; } }
 
         public DBTable(string name, List<DBField> fields)
@@ -27,6 +27,22 @@ namespace XMLParser.DB
             var primaryKey = PrimaryKeyStatement();
             var foreignKey = ForeignKeyStatement();
             return foreignKey.Length != 0 && WithForeignKeys ? $"{primaryKey} ,{foreignKey}" : primaryKey ;
+        }
+
+        public bool AddDefaultFieldToTable()
+        {
+            var defaultField = new DBField("pdfimportid", (DBFieldType.integer, 0), DBFieldKeyType.ClusteredPrimaryKey);
+            DBFields.Add(defaultField);
+            TurnAllPrimaryKeyEntrysToClusteredPrimaryKey();
+            return true;
+        }
+
+        private void TurnAllPrimaryKeyEntrysToClusteredPrimaryKey()
+        {
+            if (PrimaryKey.pkType.HasFlag(DBFieldKeyType.PrimaryKey))
+            {
+                PrimaryKey.primaryKeyFields.Select(primaryKeys => { primaryKeys.MakeClusteredPrimaryKey(); return true; }).ToList();
+            }
         }
 
         private string PrimaryKeyStatement()
